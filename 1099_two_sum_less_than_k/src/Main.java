4f560630974a9ca162bf48d8aeddaea695815c5b
mappple <mappple@qq.com>
2019-12-18 23:49:44 +0800

    0022 0046 1099

---
+++ 1099_two_sum_less_than_k/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int A[] = {34,23,1,24,75,33,54,8};
+        int K = 60;
+        System.out.println(Solution.twoSumLessThanK(A, K));
+    }
+}
